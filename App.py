2892c0576161a5b427aee20c53f78db54bc06733
Bhumikkundu001 <bhumikkundu74@gmail.com>
2025-07-25 21:29:36 +0530

    Fix insert query and download link bug

--- App.py
+++ App.py
@@ -31,14 +31,11 @@ def fetch_yt_video(link):
         return "Video"
 #
 def get_table_download_link(df, filename, text):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
+    """Generates a link allowing the data in a given panda dataframe to be downloaded"""
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    # href = f'<a href="data:file/csv;base64,{b64}">Download Report</a>'
-    return href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
+    return href
 
 
 def pdf_reader(file):
